4cfb514c51bc6c6733905d95650f144af729da0b
root <root@kali-2021-script.localdomain>
2021-04-12 17:23:55 -0400

    update README and new code based on re-wwrite using dictionary object.  Also added arguments parsing from @th3m00se

--- README.md
+++ README.md
@@ -1,5 +1,13 @@
 # hashpasscombine
 
-This is currently just POC and code is nto working properly.  Yet.
+Parse and match values from a NTLM hash dump to cracked passwords from Hashcat
+  
+Total re-write.  I am storing the orginal has dump file and the cracked hash file in a dictionary object now.
+
+You need to verify your hash dump file from DC is in the format name\\domain:hash format.
+
+The output from hashcat in hash:pass format.
+
+USAGE:  python combine.py <originaldumphashfile> <hashcatcrackedhashfile>
 
 
--- combine.py
+++ combine.py
@@ -1,24 +1,45 @@
-import sys
 import os
+import sys
+import argparse
 
+# Define Dictionary objects.
+orighashdict = {}
+crackedhashdict = {}
+
+parser = argparse.ArgumentParser(description="Parse and match values from a NTLM hash dump to cracked passwords from Hashcat")
+parser.add_argument("Input_Hash_File", help="The dump file you pulled from the DC in name\\domain:hash format")
+parser.add_argument("Cracked_Hash_File", help="The output from hashcat in hash:pass format")
+
+arguments = parser.parse_args()
+orighashfile = arguments.Input_Hash_File
+crackedhashfile = arguments.Cracked_Hash_File
 
 # Filename of prepared hash file from dump. Should be username:ntlm2 hash.
-filepath = 'cuthash.txt'
+# Import original hash file into dictionary object.
+with open(orighashfile, "r") as origin:
+    for line in origin:
+        # Set HASH as key and username as value.
+        (val, key) = line.rstrip('\n').split(':')
+        orighashdict[key] = val
 
-# Filename cracked hashes from hashhcat.
-filepath2 = 'hascatfile.txt'
+# Import cracked Hash file inot dictionary object.
+with open(crackedhashfile, "r") as hashf:
+    for line in hashf:
+        # Set HASH as key and cracked password as value.
+        (key, val) = line.rstrip('\n').split(':')
+        crackedhashdict[key] = val
 
-# Open prepared file from dump file.
-with open(filepath, 'r') as fp:
-    for line in fp:
-        # Pull username and hash and split at colon.
-        uname, hash1 = line.rstrip('\n').split(':')
-        # Open hashcat file for processing.
-        with open(filepath2, 'r') as fq:
-            for line2 in fq:
-                # Pull in hash and password from hashcat file.
-                hash2, pass1 = line2.rstrip('\r\n').split(':')
-                # Compare hash from hasscat file to hash from original dump file.
-                if hash2 == hash1:
-                    # Print out matches
-                    print("USERNAME: "+uname,"Password: "+pass1)
+#set keys for dictionary compare.
+k1 = set(orighashdict.keys())
+k2 = set(crackedhashdict.keys())
+
+# Find keys(hashes) that match in both files.
+common_keys = set(k1).intersection(set(k2))
+
+for key in common_keys:
+    if orighashdict[key] != crackedhashdict[key]:
+        
+        # Print user:pass to screen
+        print (orighashdict[key] + ":" + crackedhashdict[key])
+
+
