7618aa2689c80c7565f0f0f9ce5caddb2647871f
Th3m00se <bbailey77@gmail.com>
2021-04-13 16:26:04 -0400

    Updating to Origin

--- combine.py
+++ combine.py
@@ -1,10 +1,3 @@
-<<<<<<< HEAD
-#!/usr/bin/python
-
-import sys
-import os
-import argparse
-=======
 import os
 import sys
 import argparse
@@ -12,32 +5,10 @@ import argparse
 # Define Dictionary objects.
 orighashdict = {}
 crackedhashdict = {}
->>>>>>> upstream/main
 
 parser = argparse.ArgumentParser(description="Parse and match values from a NTLM hash dump to cracked passwords from Hashcat")
 parser.add_argument("Input_Hash_File", help="The dump file you pulled from the DC in name\\domain:hash format")
 parser.add_argument("Cracked_Hash_File", help="The output from hashcat in hash:pass format")
-<<<<<<< HEAD
-arguments = parser.parse_args()
-
-orighashfile = arguments.Input_Hash_File
-crackedhashfile = arguments.Cracked_Hash_File
-
-# Open prepared file from dump file.
-with open(orighashfile, 'r') as fp:
-    for line in fp:
-        # Pull username and hash and split at colon.
-        uname, hash1 = line.rstrip('\n').split(':')
-        # Open hashcat file for processing.
-        with open(crackedhashfile, 'r') as fq:
-            for line2 in fq:
-                # Pull in hash and password from hashcat file.
-                hash2, pass1 = line2.rstrip('\r\n').split(':')
-                # Compare hash from hashcat file to hash from original dump file.
-                if hash2 == hash1:
-                    # Print out matches
-                    print("USERNAME: "+uname,"Password: "+pass1)
-=======
 
 arguments = parser.parse_args()
 orighashfile = arguments.Input_Hash_File
@@ -72,4 +43,3 @@ for key in common_keys:
         print (orighashdict[key] + ":" + crackedhashdict[key])
 
 
->>>>>>> upstream/main
