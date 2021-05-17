6ccca451a42aa2ef2821255e1d5f69923a61c71d
k0rang <k0rang@pop-os.localdomain>
2021-05-17 14:44:41 -0500

    v3.2 final

--- combine.py
+++ combine.py
@@ -11,6 +11,8 @@ crackedhashdict = {}
 # Initialize DICT counters
 origcount = 1
 crackedhashcount = 1
+i = 1
+ii = 1
 
 parser = argparse.ArgumentParser(description="Parse and match values from a NTLM hash dump to cracked passwords from Hashcat")
 parser.add_argument("Input_Hash_File", help="The dump file you pulled from the DC in name\\domain:hash format")
@@ -38,28 +40,31 @@ with open(crackedhashfile, "r") as hashf:
         crackedhashdict[crackedhashcount] = val1,val2
         crackedhashcount = crackedhashcount +1
 
-#set keys for dictionary compare.
-#k1 = set(orighashdict.keys())
-#k2 = set(crackedhashdict.keys())
-
+# Get count of number of keys in DICT variables.
 orighashcount = len(orighashdict)
 crackedcount = len(crackedhashdict)
-print (orighashcount)
-print (crackedcount)
-#print (orighashdict)
 
-for 
-# Find keys(hashes) that match in both files.
-#common_keys = set(k1).intersection(set(k2))
-
-for key in common_keys:
-    if orighashdict[key] != crackedhashdict[key]:
-        if arguments.output:
-            out_file = arguments.output
-            print("[+] Writing output to file " + out_file + "for user: " + orighashdict[key])
-            outputstream = open(out_file, "a+")
-            outputstream.writelines(orighashdict[key] + ":" + crackedhashdict[key] + "\n")
-            outputstream.close()
+while i < (orighashcount+1):
+    ii = 1
+    while ii < (crackedhashcount):
+        oa, ob = orighashdict[i]
+        ca, cb = crackedhashdict[ii]
+        if ob == ca:
+            print (oa+':'+ob+'=='+ca+':'+cb)
+            ii = ii + 1
         else:
-            # Print user:pass to screen
-            print (orighashdict[key] + ":" + crackedhashdict[key])
+            ii = ii +1
+    i = i +1
+
+
+#for key in common_keys:
+#    if orighashdict[key] != crackedhashdict[key]:
+#        if arguments.output:
+#            out_file = arguments.output
+#            print("[+] Writing output to file " + out_file + "for user: " + orighashdict[key])
+#            outputstream = open(out_file, "a+")
+#            outputstream.writelines(orighashdict[key] + ":" + crackedhashdict[key] + "\n")
+#            outputstream.close()
+#        else:
+#            # Print user:pass to screen
+#            print (orighashdict[key] + ":" + crackedhashdict[key])
