eca8b7d4e252fd5ea1f07a630cb8103bb712b5da
k0rang <k0rang@pop-os.localdomain>
2021-05-17 10:02:38 -0400

    v3 in progress

--- combine.py
+++ combine.py
@@ -8,6 +8,10 @@ import argparse
 orighashdict = {}
 crackedhashdict = {}
 
+# Initialize DICT counters
+origcount = 1
+crackedhashcount = 1
+
 parser = argparse.ArgumentParser(description="Parse and match values from a NTLM hash dump to cracked passwords from Hashcat")
 parser.add_argument("Input_Hash_File", help="The dump file you pulled from the DC in name\\domain:hash format")
 parser.add_argument("Cracked_Hash_File", help="The output from hashcat in hash:pass format")
@@ -21,23 +25,32 @@ crackedhashfile = arguments.Cracked_Hash_File
 # Import original hash file into dictionary object.
 with open(orighashfile, "r") as origin:
     for line in origin:
-        # Set HASH as key and username as value.
-        (val, key) = line.rstrip('\n').split(':')
-        orighashdict[key] = val
+        # Set NAME as key and HASH as value.
+        (val1, val2) = line.rstrip('\n').split(':')
+        orighashdict[origcount] = val1,val2
+        origcount = origcount + 1
 
-# Import cracked Hash file inot dictionary object.
+# Import cracked Hash file into dictionary object.
 with open(crackedhashfile, "r") as hashf:
     for line in hashf:
         # Set HASH as key and cracked password as value.
-        (key, val) = line.rstrip('\n').split(':')
-        crackedhashdict[key] = val
+        (val1, val2) = line.rstrip('\n').split(':')
+        crackedhashdict[crackedhashcount] = val1,val2
+        crackedhashcount = crackedhashcount +1
 
 #set keys for dictionary compare.
-k1 = set(orighashdict.keys())
-k2 = set(crackedhashdict.keys())
+#k1 = set(orighashdict.keys())
+#k2 = set(crackedhashdict.keys())
 
+orighashcount = len(orighashdict)
+crackedcount = len(crackedhashdict)
+print (orighashcount)
+print (crackedcount)
+#print (orighashdict)
+
+for 
 # Find keys(hashes) that match in both files.
-common_keys = set(k1).intersection(set(k2))
+#common_keys = set(k1).intersection(set(k2))
 
 for key in common_keys:
     if orighashdict[key] != crackedhashdict[key]:
